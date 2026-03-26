e56dced34ce8a06886b6db98c7c24de098edf03b
RoflikBEST <rocer.slesh@gmail.com>
2026-03-26 15:50:11 +0300

    add: entity

---
+++ entity/src/engine/shared/config_domains.h
@@ -0,0 +1,16 @@
+// This file can be included several times.
+
+#ifndef CONFIG_DOMAIN
+#error "CONFIG_DOMAIN macro not defined"
+#define CONFIG_DOMAIN(Name, ConfigPath, HasVars) ;
+#endif
+
+CONFIG_DOMAIN(DDNET, "settings_ddnet.cfg", true)
+CONFIG_DOMAIN(ENTITY, "settings_entity.cfg", true)
+// E-Client
+CONFIG_DOMAIN(ENTITYBINDHWEEL, "entity_bindwheel.cfg", false)
+CONFIG_DOMAIN(ENTITYQUICKACTIONS, "entity_quickactions.cfg", false)
+// T-Client
+CONFIG_DOMAIN(TCLIENTPROFILES, "tclient_profiles.cfg", false)
+CONFIG_DOMAIN(TCLIENTCHATBINDS, "tclient_chatbinds.cfg", false)
+CONFIG_DOMAIN(TCLIENTWARLIST, "tclient_warlist.cfg", false)
